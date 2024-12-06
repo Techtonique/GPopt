58773935512d8b6d1584d43e62f0dbde0d07b10c
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-12-06 20:15:49 +0100

    use method='splitconformal' in predict

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 subprocess.call("pip install -r requirements.txt", shell=True)
 
